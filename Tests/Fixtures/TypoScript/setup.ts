0955e82c240a9a06bab8b114218999c546d79d56
r3h6 <r3h6@outlook.com>
2017-06-01 22:34:22 +0200

    [TASK] Add setup fixture

---
+++ Tests/Fixtures/TypoScript/setup.ts
@@ -0,0 +1,6 @@
+# Execute this setup to render the test cases.
+page >
+page = PAGE
+page.typeNum = 0
+page.10 = FLUIDTEMPLATE
+page.10.file = typo3conf/ext/image_async_process/Tests/Fixtures/Templates/Test.html
